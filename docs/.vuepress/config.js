010931abea4031e7044632335d64d22ffe413513
chenbing <chenbingbing@dazuhang.com>
2020-07-29 17:18:53 +0800

    初始化vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,65 @@
+module.exports = {
+    title: '',
+    description: '是起点不是终点',
+    configureWebpack: {
+        resolve: {
+          alias: {
+            '@image': 'assets/images'
+          }
+        }
+      },
+
+      themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+          ],
+
+        sidebar: [
+          {
+              title: '序章',
+              path: '/intro',
+              collapsable: false, // 可选的, 默认值是 true,
+              sidebarDepth: 1,    // 可选的, 默认值是 1
+              children: [
+                  '/'
+              ]
+          },
+          {
+            title: '前端',
+            path: '/frontend',
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1,    // 可选的, 默认值是 1
+            children: [
+                '/'
+            ] 
+          },
+          {
+            title: '后端',
+            path: '/backend',
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1,    // 可选的, 默认值是 1
+            children: [
+                '/'
+            ] 
+          },
+          {
+            title: '运维部署',
+            path: '/DevOps',
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1,    // 可选的, 默认值是 1
+            children: [
+                'nginx'
+            ] 
+          },
+          {
+            title: '工具',
+            path: '/tools',
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1,    // 可选的, 默认值是 1
+            children: [
+                '/tools/Mac自用安装软件' 
+            ] 
+          },
+        ]
+      }
+  }
---
+++ docs/tools/Mac自用安装软件.md
@@ -0,0 +1,31 @@
+# Mac自用安装软件
+1. [Alfread](https://www.alfredapp.com/) - 通过关键字快速打开软件/搜索 
+2. [iTerm](https://www.iterm2.com/) - 替代系统自带终端,配合[ZSH](https://ohmyz.sh/)使用更佳
+3. [MWeb](https://zh.mweb.im/) - markdown语法写作记录工具
+4. [Dash](https://kapeli.com/dash) - 文档查看工具
+5. [SecureCRT](https://www.vandyke.com/products/securecrt/) - 连接服务器的SSH终端
+6. [Navicat](https://www.navicat.com.cn/) - 连接数据库可视化工具
+7. [Sourcetree ](https://www.sourcetreeapp.com/) - git可视化客户端
+8. [Moom](https://manytricks.com/moom/) - 窗口大小布局管理 
+9. [ImageOptim](https://imageoptim.com/mac) - 图片压缩工具
+10. [XMind](https://www.xmind.cn/) - 思维导图
+11. [Charles](https://www.charlesproxy.com/) - 抓包工具,可以用来抓取接口,借鉴数据结构
+12. [Postman](https://www.getpostman.com/) - API接口请求调试工具
+13. [ShadowsocksX](https://github.com/shadowsocks/ShadowsocksX-NG/releases) - ss代理客户端
+14. [Homebrew](https://brew.sh/) - MacOS包管理工具,可安装软件/第三方包
+15. [Foxmail](https://www.foxmail.com/) - 邮件管理客户端
+16. [欧路词典](https://www.eudic.net/v4/en/app/eudic) - 英语词典软件
+17. [Snip](https://snip.qq.com/) - 截图工具
+18. [1Password](https://1password.com/) - 密码管理工具
+19. [PxCook](https://www.fancynode.com.cn/pxcook)和[马克鳗](http://www.getmarkman.com/) - 可对设计稿PSD或者图片进行标注
+20. [Etcher](https://www.balena.io/etcher/) - 将系统镜像iso/img写入SD卡/U盘/硬盘等存储介质
+21. [Impactor](http://www.cydiaimpactor.com/) - 将ipa文件安装到iPhone上,安装过程中需要苹果开发者账号
+
+
+
+#### 更全面的指南:
+[Awesome Mac](https://wangchujiang.com/awesome-mac/index.zh.html)
+
+[少数派](https://sspai.com/tag/Mac)
+
+[Mac应用程序无法打开或文件损坏的处理方法](https://www.macbl.com/article/how-to/app-is-damaged-and-can't-be-opened)
