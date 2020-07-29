c049f263ec098a6c94bed08572a2870c5068a89a
chenbing <chenbingbing@dazuhang.com>
2020-07-29 18:42:41 +0800

    添加新文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -48,7 +48,7 @@ module.exports = {
             collapsable: false, // 可选的, 默认值是 true,
             sidebarDepth: 1,    // 可选的, 默认值是 1
             children: [
-                'nginx'
+                '/'
             ] 
           },
           {
